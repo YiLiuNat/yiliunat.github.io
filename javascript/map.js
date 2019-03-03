9c8bb2b9972cfef9b10ba6970f03c05270f8a094
Yi Liu <1009066669@qq.com>
2019-03-03 23:39:49 +0000

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,5 +1,5 @@
 // $(document).ready(function() {
-	
+try{
 var map;
 var faisalabad = {lat:52.449970, lng:-1.930870};
 var navDest = "1.01, 1.01";
@@ -969,4 +969,7 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
           }
       });
   });
+}
+}catch(e){
+	alert(e);
 }
