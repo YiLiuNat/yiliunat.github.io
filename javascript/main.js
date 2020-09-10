ec8f4fc241740f591c1d20670bae8f08501e6851
Yi Liu - Nat <1009066669@qq.com>
2020-09-10 22:57:46 +0800

    update

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,8 @@
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById("example")
+	<h1>Hello, world!</h1>,
+	document.getElementById('nat')
 );
+
 
 // ReactDom.render(
 // 	<h1>Nat</h1>,
