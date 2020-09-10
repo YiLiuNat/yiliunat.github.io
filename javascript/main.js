2c7839b9ce9070165421297b394c8607bbf89b31
Yi Liu - Nat <1009066669@qq.com>
2020-09-10 22:33:09 +0800

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,9 @@
-ReactDom.render(
-	<h1>Nat</h1>,
-	document.getElementById('nat')
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('example')
 );
+
+// ReactDom.render(
+// 	<h1>Nat</h1>,
+// 	document.getElementById('nat')
+// );
