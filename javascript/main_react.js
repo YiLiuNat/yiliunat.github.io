7c0bc365530280859229dee638007aa4a61c811e
Yi Liu - Nat <1009066669@qq.com>
2021-02-06 17:01:11 +0800

    Update main_react.js

--- javascript/main_react.js
+++ javascript/main_react.js
@@ -1,19 +1,37 @@
-function Nat(props) {
-	return (
-		<div>
-			<h1>Hello, Nat!</h1>
-			<h2>icon</h2>
-			<a href="/fyp">Final Year Project</a>
-		</div>
-	);
+class Main extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello, Nat!</h1>
+				<h2>Yi Liu</h2>
+				<a href="/fyp">Final Year Project</a>
+			</div>
+		);
+	}
 }
 
+ReactDOM.render(
+	<Main />,
+	document.getElementById('nat')
+);
 
 
-function render() {
-	ReactDOM.render(
-		Nat(),
-		document.getElementById('nat')
-	);
-}
-render();
+// function Nat(props) {
+// 	return (
+// 		<div>
+// 			<h1>Hello, Nat!</h1>
+// 			<h2>Yi Liu</h2>
+// 			<a href="/fyp">Final Year Project</a>
+// 		</div>
+// 	);
+// }
+
+
+
+// function render() {
+// 	ReactDOM.render(
+// 		Nat(),
+// 		document.getElementById('nat')
+// 	);
+// }
+// render();
