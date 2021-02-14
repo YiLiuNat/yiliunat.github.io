c81ddb288345e5b5c5472e0b02c4ac28a703d140
Yi Liu - Nat <1009066669@qq.com>
2021-02-14 19:15:15 +0800

    Update main_react.js

--- src/javascript/main_react.js
+++ src/javascript/main_react.js
@@ -13,8 +13,15 @@ class Title extends React.Component {
 		return (
 			<div id="title">
 				<h1>Hello, Nat!</h1>
-				<h2> Yi Liu </h2> 
-				<a href = "/fyp" > Final Year Project </a>
+				<h2> Yi Liu </h2>
+				<div id="fypTitle"> 
+					<a href = "/fyp" > Final Year Project </a>
+					<div id="fypDetail">
+						<p>Researched mainly on the online-map interaction topics, with JavaScript as the main programming language. Provided an integrated solution for student timetables and
+campus path planning system via the user-centred design; Planed a path automatically for students to reach their lecture buildings based on their timetable.</p>
+					</div>
+				</div>
+				 
 			</div>
 		);
 	}
