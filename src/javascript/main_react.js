7c83e4947cab84be2391a0a70becdf57a392aaee
Yi Liu - Nat <1009066669@qq.com>
2021-02-16 22:59:13 +0800

    Update main_react.js

--- src/javascript/main_react.js
+++ src/javascript/main_react.js
@@ -9,6 +9,24 @@ class Title extends React.Component {
 	}
 }
 
+class Project extends React.Component {
+	render() {
+		return (
+			<div id="fypProject"> 
+				<div id="fypTitle">
+					<a href = "/fyp" target="_blank"> Final Year Project </a>
+				</div>
+				<div id="fypDetail">
+					<p>Researched mainly on the online-map interaction topics, with JavaScript as the main programming language. Provided an integrated solution for student timetables and
+		campus path planning system via the user-centred design; Planed a path automatically for students to reach their lecture buildings based on their timetable.</p>
+					<p>Developed an online mobile web-app, which has been designed to behave like a native mobile app (compatible with both Android and iOS system); Allowed visitors to navigate university’s landmarks or buildings, and enabled students to upload their timetables for an automatically updated map that helped them plan paths for their next lectures.</p>
+
+				</div>
+			</div>
+		);
+	}
+}
+
 class Main extends React.Component {
 	constructor(props) {
 		super(props);
@@ -110,23 +128,6 @@ class Main extends React.Component {
 // 	}
 // }
 
-class Project extends React.Component {
-	render() {
-		return (
-			<div id="fypProject"> 
-				<div id="fypTitle">
-					<a href = "/fyp" > Final Year Project </a>
-				</div>
-				<div id="fypDetail">
-					<p>Researched mainly on the online-map interaction topics, with JavaScript as the main programming language. Provided an integrated solution for student timetables and
-		campus path planning system via the user-centred design; Planed a path automatically for students to reach their lecture buildings based on their timetable.</p>
-					<p>Developed an online mobile web-app, which has been designed to behave like a native mobile app (compatible with both Android and iOS system); Allowed visitors to navigate university’s landmarks or buildings, and enabled students to upload their timetables for an automatically updated map that helped them plan paths for their next lectures.</p>
-
-				</div>
-			</div>
-		);
-	}
-}
 
 ReactDOM.render(
 	<Main />,
