7a347028876eeddb8b9f717bc88885ed2de6d76a
Yi Liu - Nat <1009066669@qq.com>
2021-02-18 23:20:03 +0800

    Update main_react.js

--- src/javascript/main_react.js
+++ src/javascript/main_react.js
@@ -111,7 +111,7 @@ class Main extends React.Component {
 					</button>
 				</div>
 				
-				{(window.location.hash == "#!projects")?(<Projects />):null}
+				{(window.location.hash == "#!projects")?(document.title="Projects - Yi Liu 项目", <Projects />):null}
 				{/*showProjects ? (<Projects />) : null*/}
 				{(window.location.hash == "")?(<Title />):null}
 
